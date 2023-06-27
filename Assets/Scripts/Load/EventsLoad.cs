5b2f861dab503ae6c197735f4a7c0b61f242fab3
SunYuanHen <henry10701@gmail.com>
2023-06-27 16:09:41 +0800

    20230627
    更改了很多細節和整個架構調整

---
+++ Assets/Scripts/Load/EventsLoad.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsLoad : MonoBehaviour
+{
+    [SerializeField] GameObject eventA,eventB,eventC;
+    //int r = Random.Range(0, 3);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/World/BattleEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleEvent : MonoBehaviour
+{
+    public void Battle()
+    {
+
+    } 
+}
