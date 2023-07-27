cea222607d118ab070a77349a2db4eb41daf2187
孫源亨 <79952281+SunYuanHen@users.noreply.github.com>
2023-07-27 15:11:10 +0800

    20230727
    調整讀取檔案方式

--- Assets/Scripts/Stage/Stage.cs
+++ Assets/Scripts/Stage/Stage.cs
@@ -6,21 +6,23 @@ public class Stage : MonoBehaviour
 {
     public GameObject sceneLoader;
     int stage = 0;
-    string json;
-    Log log = new("");
+    string[] log ={
+        "你沒有錢\n你唯一的財物是穿在身上的內褲\n" +
+            "一到酒館老闆給了你一張地圖標著有賞金的怪物\n為了錢\n你只好踏上新的冒險...",
+        "你有了點錢\n你身上的財物多了個上衣\n隔天到酒館老闆又給了你一張地圖標著有賞金的怪物" +
+            "\n為了錢\n你又踏上新的冒險...",
+        "你有錢了\n某天在逛街的路上\n被酒館老闆的跑車撞死了\n很可惜\n你的人生結束了..."
+    };
     void Awake()
     {
         stage = PlayerPrefs.GetInt("stage");
-        string filePath = $"/Scripts/Stage/log{stage}.json";
-        string fullPath = (Application.dataPath + filePath);
-        json = File.ReadAllText(fullPath);
-        log = JsonUtility.FromJson<Log>(json);
+        
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
             NextScene();
-        GetComponent<Text>().text = log.log.Replace("\\n","\n");
+        GetComponent<Text>().text = log[stage - 1];
     }
     
     public void NextScene()
--- Assets/Scripts/Stage/log.cs
+++
@@ -1,8 +0,0 @@
-public class Log
-{
-    public string log;
-    public Log(string log)
-    {
-        this.log = log;
-    }
-}
