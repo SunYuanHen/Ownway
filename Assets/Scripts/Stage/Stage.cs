c488366f17a3827b1d7c5a8f3c774b717fe850b9
孫源亨 <henry10701@gmail.com>
2023-09-11 12:45:03 +0800

    20230911
    正在重新調整內容

--- Assets/Scripts/Stage/Stage.cs
+++ Assets/Scripts/Stage/Stage.cs
@@ -1,17 +1,14 @@
-using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
- 
+
 public class Stage : MonoBehaviour
 {
     public GameObject sceneLoader;
     int stage = 0;
     string[] log ={
-        "你沒有錢\n你唯一的財物是穿在身上的內褲\n" +
-            "一到酒館老闆給了你一張地圖標著有賞金的怪物\n為了錢\n你只好踏上新的冒險...",
-        "你有了點錢\n你身上的財物多了個上衣\n隔天到酒館老闆又給了你一張地圖標著有賞金的怪物" +
-            "\n為了錢\n你又踏上新的冒險...",
-        "你有錢了\n某天在逛街的路上\n被酒館老闆的跑車撞死了\n很可惜\n你的人生結束了..."
+        "第一關:一到酒館，老闆給了你一張地圖。\n標著有賞金的怪物\n你踏上新的冒險...",
+        "第二關:一到酒館，老闆又給了你一張地圖。\n標著有賞金的新怪物\n你踏上新的冒險...",
+        "恭喜破關!!"
     };
     void Awake()
     {
--- Assets/Scripts/World/ControlText.cs
+++ Assets/Scripts/World/ControlText.cs
@@ -7,22 +7,11 @@ public class ControlText : MonoBehaviour
     string[] text = {
         "W or ↑ :往上\nA  or ←:往左\nS  or ↓ :往下\nD  or →:往右",
         "Hp : 生命\nAtk : 攻擊\nDef : 防禦\nSpd : 速度",
-        "戰鬥傷害:ATK-DEF\n再爛傷害都不會\n低於1",
+        "戰鬥傷害:ATK-DEF\n傷害不會低於1",
         "速度(SPD)較快者先攻",
         "寶箱有時\n會得到能力\n有時啥都沒有\n有時...",
         "R鍵可以回城\n不過地圖也會重置",
-        "對手名子爛\n不代表它爛",
-        "你還期待有\n特別的提示?\n當然沒有",
-        "王不是\n第一次就能贏\n請善用回城(R)",
-        "如果覺得目前\n打不贏王\n請考慮回城(R)",
-        "勝敗乃兵家常事\n但你不是兵",
-        "是不是都在\n看提示?你真閒",
-        "難度太高?\n可能是你\n運氣不好?",
-        "沒有風險\n沒有收穫",
-        "怕死?回城\n打不贏王?回城",
-        "開場就掛?\n你也太衰...",
-        "戰鬥是全自動\n只要等贏(死)\n就好",
-        "有沒有覺得\n自動存檔是好功能?\n沒錯!這裡沒有"
+        "如果覺得目前\n打不贏王\n試試回城(R)"
     };
 
     void Update()
@@ -30,8 +19,7 @@ public class ControlText : MonoBehaviour
         if (Input.anyKeyDown)
         {
             n = Random.Range(0,text.Length);
-            while(n == num)
-                n = Random.Range(0, text.Length);
+            while(n == num) n = Random.Range(0, text.Length);
             SetText();
         }
     }
